35cc447f7c6c1d1e30941deccf11a64d1d877e3e
Dirk Tony <tonyramanantsalama@gmail.com>
2026-07-15 12:26:02 +0300

    Update install-arm64-cross-build-deps.sh

--- scripts/install-arm64-cross-build-deps.sh
+++ scripts/install-arm64-cross-build-deps.sh
@@ -12,14 +12,14 @@ apt-get install -y \
   file:arm64 \
   libssl-dev:arm64 \
   libgtk-3-dev:arm64 \
-  libappindicator3-dev \
+  libappindicator3-dev:arm64 \
   xdg-utils:arm64 \
   librsvg2-dev:arm64
 
 apt-get install -y \
-  libwebkit2gtk-4.1-0=2.44.0-2:arm64 \
-  libwebkit2gtk-4.1-dev=2.44.0-2:arm64 \
-  libjavascriptcoregtk-4.1-0=2.44.0-2:arm64 \
-  libjavascriptcoregtk-4.1-dev=2.44.0-2:arm64 \
-  gir1.2-javascriptcoregtk-4.1=2.44.0-2:arm64 \
-  gir1.2-webkit2-4.1=2.44.0-2:arm64
+  libwebkit2gtk-4.1-0:arm64=2.44.0-2 \
+  libwebkit2gtk-4.1-dev:arm64=2.44.0-2 \
+  libjavascriptcoregtk-4.1-0:arm64=2.44.0-2 \
+  libjavascriptcoregtk-4.1-dev:arm64=2.44.0-2 \
+  gir1.2-javascriptcoregtk-4.1:arm64=2.44.0-2 \
+  gir1.2-webkit2-4.1:arm64=2.44.0-2
